de5496bd567795816cec78000cec4b0910cfc9ae
BastienLairis <bastien.lairis@gmail.com>
2022-11-18 11:46:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Header />
       <h1>Hello WorkShop Git Github</h1>
       <p>Lorem ipsum dolor sit amet consectetur</p>
+      <p>Code édité depuis Github</p>
     </div>
   );
 }
