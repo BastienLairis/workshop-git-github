404b5b5074b8ebdf5d4a50291b991f16bb12f32d
bastienlairis <bastien.lairis@gmail.com>
2022-11-18 11:22:38 +0100

    add component header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 
+//components
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello WorkShop Git Github</h1>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header
+      style={{
+        background: "salmon",
+        height: "100px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h2>Je suis le header</h2>
+    </header>
+  );
+};
+
+export default Header;
